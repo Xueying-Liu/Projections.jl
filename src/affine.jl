64cb6c207b46373dd6ee17d0c741f3a362bf9f7c
Xueying-Liu <52796684+Xueying-Liu@users.noreply.github.com>
2019-07-11 15:11:38 -0400

    Update affine.jl

--- src/affine.jl
+++ src/affine.jl
@@ -6,9 +6,8 @@ struct Affine{T <: Real} <: ConvexSet
 end
 
 function project!(s::Affine{T}, v::Vector{T}, y::Vector{T}) where {T <: Real}
-    pA = zeros(size(transpose(s.A)))
-    pA = pinv(s.A)
-    v .= y. - pA .* (s.A .* y .- s.b)
+  
+    v .= y. - pinv(s.A) .* (s.A .* y .- s.b)
 end
 
 function project(s::Affine{<:Real}, y::Vector{<:Real})
