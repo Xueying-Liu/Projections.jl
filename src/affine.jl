6371594db621323a81f41f56b804d62b131de1c3
Xueying-Liu <52796684+Xueying-Liu@users.noreply.github.com>
2019-07-11 15:47:23 -0400

    Update affine.jl

--- src/affine.jl
+++ src/affine.jl
@@ -1,5 +1,5 @@
 export Affine
-using LinearAlgebra
+
 struct Affine{T <: Real} <: ConvexSet
     A::Matrix{T}
     b::Vector{T}
@@ -7,7 +7,7 @@ end
 
 function project!(s::Affine{T}, v::Vector{T}, y::Vector{T}) where {T <: Real}
   
-    v .= y. - pinv.(s.A) .* (s.A .* y .- s.b)
+    v .= y. - inv.(s.A) .* (s.A .* y .- s.b)
 end
 
 function project(s::Affine{<:Real}, y::Vector{<:Real})
