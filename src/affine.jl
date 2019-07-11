7b04c971a62a535af18cc06b1aa93782661bb579
Xueying-Liu <52796684+Xueying-Liu@users.noreply.github.com>
2019-07-11 14:39:21 -0400

    add affine projection

---
+++ src/affine.jl
@@ -0,0 +1,16 @@
+export Affine
+
+struct Affine{T <: Real} <: ConvexSet
+    A::Matrix{T}
+    b::Vector{T}
+end
+
+function project!(s::Affine{T}, v::Vector{T}, y::Vector{T}) where {T <: Real}
+    
+    pA = pinv(s.A)
+    v .= y. - pA .* (s.A .* y .- s.b)
+end
+
+function project(s::Affine{<:Real}, y::Vector{<:Real})
+    project!(s, similar(y), y)
+end
