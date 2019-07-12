97efe64686dab0a3b709213557b4eb97b637fadb
Xueying-Liu <52796684+Xueying-Liu@users.noreply.github.com>
2019-07-12 14:33:25 -0400

    add finite projection

--- src/finite.jl
+++ src/finite.jl
@@ -4,11 +4,11 @@ struct Finite{T <: Real} <: ConvexSet
     x::Matrix{T}
 end
 
-using Distances, LinearAlgebra
+using Distances
 function project!(s::Finite{<:Real}, v::Vector{T}, y::Vector{T}) where {T <: Real}
-    dist = LinearAlgebra.colwise(Distances.SqEuclidean(), s.x, y)
+    dist = Distances.colwise(Distances.SqEuclidean(), s.x, y)
     j = argmin(dist)
-    v = x[:, j]
+    v = s.x[:, j]
 end
 
 function project(s::Finite{<:Real}, y::Vector{<:Real})
