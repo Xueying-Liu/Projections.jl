145e259221d692b20ef540d9c8c8163d098dffb0
Xueying-Liu <52796684+Xueying-Liu@users.noreply.github.com>
2019-07-12 12:04:53 -0400

    add affine projection

--- src/affine.jl
+++ src/affine.jl
@@ -5,6 +5,8 @@ struct Affine{T <: Real}
     b::Vector{T}
 end
 
+using LinearAlgebra
+
 function project!(s::Affine{T}, v::Vector{T}, y::Vector{T}) where {T <: Real}
     v = y .- LinearAlgebra.pinv(s.A) * (s.A * y .- s.b)
 end
