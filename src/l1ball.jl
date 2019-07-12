d0b3cc1676d5ca4be41c11ef75e61523050a3d6b
Xueying-Liu <52796684+Xueying-Liu@users.noreply.github.com>
2019-07-12 16:35:46 -0400

    add L1 ball projection

---
+++ src/l1ball.jl
@@ -0,0 +1,21 @@
+export L1ball
+
+struct L1ball{T <: Real} <: ConvexSet
+    radius::T
+    center::Vector{T}
+end
+Simplex() = Simplex(1.0)
+
+function project!(s::L1ball{T}, v::Vector{T}, y::Vector{T}) where {T <: Real}
+    z = sum(abs, y-s.center)
+   if z <= s.radius
+      v = y
+   else
+      p = Project(Simplex(s.radius),y)
+      v = s.center+sign(y-s.center).*p
+   end
+end
+
+function project(s::L1ball{<:Real}, y::Vector{<:Real})
+    project!(s, similar(y), y)
+end
