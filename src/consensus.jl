b78dae657b43afd627475f99c3b99920486f1f49
Xueying-Liu <52796684+Xueying-Liu@users.noreply.github.com>
2019-07-12 13:18:01 -0400

    add concensus projection

---
+++ src/consensus.jl
@@ -0,0 +1,16 @@
+export Simplex
+
+struct Consensus{T <: Real} 
+    index::T
+end
+
+Consensus() = Consensus(0.0)
+
+function project!(s::Consensus{<:Real}, v::Matrix{T}, y::Matrix{T}) where {T <: Real}
+  v = repeat(mean(y, dims = 2), inner = [1, size(y, 2)])
+end
+
+
+function project(s::Consensus{<:Real}, y::Matrix{<:Real})
+    project!(s, similar(y), y)
+end
