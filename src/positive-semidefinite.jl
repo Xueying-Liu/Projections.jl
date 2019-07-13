67cfc2da822866d8f74b54b79542414d7bc136d4
Xueying-Liu <52796684+Xueying-Liu@users.noreply.github.com>
2019-07-13 12:32:55 -0400

    add positive semidefinite cone projection

---
+++ src/positive-semidefinite.jl
@@ -0,0 +1,24 @@
+export PositiveSemidefinite
+
+struct PositiveSemidefinite{T <: Real} <: ConvexSet
+    r::T
+end
+PositiveSemidefinite() = PositiveSemidefinite(0.0)
+```
+This function projects the symmetric matrix Y onto the closest positive definite matrix.
+```
+
+function project!(s::PositiveSemidefinite{<:Real}, v::Matrix{T}, y::Matrix{T}) where {T <: Real}
+   (D,V) = LinearAlgebra.eigen(y)
+   X = zeros(size(y))
+   for j = 1:size(y,1)
+      if real(D[j]) >= 0.0
+         X = X .+ V[:,j] * D[j] * V[:,j]'
+      end   
+   end
+   v = X
+end
+
+function project(s::PositiveSemidefinite{<:Real}, y::Matrix{<:Real})
+    project!(s, similar(y), y)
+end
