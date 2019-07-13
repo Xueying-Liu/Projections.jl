34607c95403d5bf7b99fa6dd5309c897071f16c0
Xueying-Liu <52796684+Xueying-Liu@users.noreply.github.com>
2019-07-13 11:04:56 -0400

    add low rank projection

---
+++ src/low-rank-matrix.jl
@@ -0,0 +1,20 @@
+export Lowrank
+
+struct Lowrank{T <: Real} <: ConvexSet
+    k::T
+end
+```
+This function projects the matrix y onto the closest matrix of rank k or less.
+```
+function project!(s::Lowrank{<:Real}, v::Matrix{T}, y::Matrix{T}) where {T <: Real}
+    U, S, V = svd(y)
+    Z = zeros(y)
+    for j = 1:s.k
+      Z = Z+S[j]*U[:,j]*V[:,j]'   
+    end
+    v = Z
+end
+
+function project(s::Lowrank{<:Real}, y::Vector{<:Real})
+    project!(s, similar(y), y)
+end
