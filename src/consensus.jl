60c2da6596014c45615fd6bb7a2dae1d6c77ece0
Xueying-Liu <52796684+Xueying-Liu@users.noreply.github.com>
2019-07-15 10:50:41 -0400

    add consensus projection

--- src/consensus.jl
+++ src/consensus.jl
@@ -5,7 +5,6 @@ struct Consensus{T <: Real}
 end
 
 Consensus() = Consensus(0.0)
-using Statistics
 
 function project!(s::Consensus{<:Real}, v::Matrix{T}, y::Matrix{T}) where {T <: Real}
   v = repeat(Statistics.mean(y, dims = 2), inner = [1, size(y, 2)])
